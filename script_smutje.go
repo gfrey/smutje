21107b18955ad3a59b1a28df6a1cc605ee5928ec
Gereon Frey <me@gereonfrey.de>
2016-06-18 17:23:23 +0200

    adds support for downloading jenkins artifacts
    The artifact is only loaded if the fingerprint changed.

--- script_smutje.go
+++ script_smutje.go
@@ -1,15 +1,18 @@
 package smutje
 
 import (
+	"bytes"
 	"crypto/md5"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/gfrey/smutje/connection"
 	"github.com/gfrey/smutje/logger"
+	"github.com/pkg/errors"
 )
 
 type smutjeScript struct {
@@ -50,10 +53,96 @@ func (s *smutjeScript) initCommands(attrs smAttributes) error {
 		s.Command, err = newExecWriteFileCmd(s.Path, args[1:])
 	case ":write_template":
 		s.Command, err = newExecWriteTemplateCmd(s.Path, args[1:])
+	case ":jenkins_artifact":
+		s.Command, err = newJenkinsArtifactCmd(args[1:])
 	default:
 		return fmt.Errorf("command %s unknown", args[0])
 	}
 	return err
+}
+
+type execJenkinsArtifactCmd struct {
+	Host     string
+	Job      string
+	Artifact string
+	Target   string
+	Owner    string
+	Umask    string
+
+	hash string
+	url  string
+}
+
+func newJenkinsArtifactCmd(args []string) (*execJenkinsArtifactCmd, error) {
+	if len(args) < 4 || len(args) > 6 {
+		return nil, fmt.Errorf(`syntax error: jenkins artifact usage ":jenkins_artifact <host> <job> <artifact> <target> [<user> <umask>]?"`)
+	}
+
+	cmd := new(execJenkinsArtifactCmd)
+	cmd.Host, cmd.Job, cmd.Artifact, cmd.Target = args[0], args[1], args[2], args[3]
+	cmd.Owner, cmd.Umask = "root", "0644"
+	if len(args) > 4 {
+		cmd.Owner = args[4]
+	}
+	if len(args) == 6 {
+		cmd.Umask = args[5]
+	}
+
+	return cmd, nil
+}
+
+func (a *execJenkinsArtifactCmd) Hash() string {
+	return a.hash
+}
+
+func (a *execJenkinsArtifactCmd) Prepare(attrs smAttributes, prevHash string) (string, error) {
+	a.url = fmt.Sprintf("http://%s/job/%s/lastSuccessfulBuild/artifact/%s", a.Host, a.Job, a.Artifact)
+	resp, err := http.Get(a.url + "/*fingerprint*/")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, resp.Body); err != nil {
+		return "", err
+	}
+
+	parts := strings.Split(buf.String(), "MD5: ")
+	if len(parts) != 2 {
+		return "", errors.New("failed to read artifact fingerprint")
+	}
+	fingerprint := strings.SplitN(parts[1], " ", 2)[0]
+
+	hash := md5.New()
+	if _, err := hash.Write([]byte(prevHash + a.Host + a.Job + a.Artifact + fingerprint)); err != nil {
+		return "", errors.Wrap(err, "failed to create command hash")
+	}
+	a.hash = fmt.Sprintf("%x", hash.Sum(nil))
+
+	return a.hash, nil
+}
+
+func (a *execJenkinsArtifactCmd) Exec(l logger.Logger, client connection.Client) error {
+	sess, err := client.NewLoggedSession(l)
+	if err != nil {
+		return err
+	}
+	defer sess.Close()
+
+	l.Printf("downloading file %q from %q", a.Target, a.url)
+	setFilePerms := ""
+	// TODO is possible to set only one of the both?
+	if a.Owner != "" && a.Umask != "" {
+		setFilePerms = " && chown " + a.Owner + " %[1]s && chmod " + a.Umask + " %[1]s"
+	}
+
+	cmd := fmt.Sprintf(`bash -c "{ dir=$(dirname %[1]s); test -d \${dir} || mkdir -p \${dir}; } && curl -sSL %[2]s -o %[1]s`+setFilePerms+`"`, a.Target, a.url)
+	if err := sess.Start(cmd); err != nil {
+		return err
+	}
+
+	return sess.Wait()
 }
 
 type execWriteFileCmd struct {
