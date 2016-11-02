bcc441ac295a7d41b8d6dde0813ea7880f728955
Gereon Frey <me@gereonfrey.de>
2016-11-02 12:08:44 +0100

    Add basic password injection
    This is some rough idea how to do this. Currenlty passwords are read
    from a `.passwords` file in the working directory. This should be
    improved to rely on other sources.

--- script_smutje.go
+++ script_smutje.go
@@ -13,6 +13,8 @@ import (
 	"github.com/gfrey/smutje/connection"
 	"github.com/gfrey/smutje/logger"
 	"github.com/pkg/errors"
+	"bufio"
+	"strconv"
 )
 
 type smutjeScript struct {
@@ -59,10 +61,134 @@ func (s *smutjeScript) initCommands(attrs smAttributes) error {
 		s.Command, err = newExecWriteTemplateCmd(s.Path, args[1:])
 	case ":jenkins_artifact":
 		s.Command, err = newJenkinsArtifactCmd(args[1:])
+	case ":inject_passwords":
+		s.Command, err = newInjectPasswordsCmd(args[1:])
 	default:
 		return errors.Errorf("command %s unknown", args[0])
 	}
 	return err
+}
+
+type execInjectPasswordsCmd struct {
+	Passwords []string
+
+	values map[string]string
+	hash string
+
+	cache map[string]string
+}
+
+func newInjectPasswordsCmd(args []string) (*execInjectPasswordsCmd, error) {
+	if len(args) == 0 {
+		return nil, errors.Errorf(`syntax error: password injector usage ":inject_password [<password_name>]+"`)
+	}
+
+	cmd := new(execInjectPasswordsCmd)
+	cmd.Passwords = args
+	cmd.values = make(map[string]string, len(args))
+	return cmd, nil
+}
+
+func (a *execInjectPasswordsCmd) Hash() string {
+	return a.hash
+}
+
+func (a *execInjectPasswordsCmd) Prepare(attrs smAttributes, prevHash string) (string, error) {
+	hash := md5.New()
+	if _, err := hash.Write([]byte(prevHash)); err != nil {
+		return "", errors.Wrap(err, "failed to write hash")
+	}
+
+	for _, pwdName := range a.Passwords {
+		pwd, err := a.getPassword(pwdName)
+		if err != nil {
+			return "", err
+		}
+		a.values[pwdName] = pwd
+		if _, err := hash.Write([]byte(pwd)); err != nil {
+			return "", errors.Wrap(err, "failed to write hash")
+		}
+
+		attrs["PASSWORD_"+pwdName] = fmt.Sprintf(`$(grep %s /tmp/smutje/passwords | cut -f2)`, pwdName)
+		attrs["PASSWORD_"+pwdName+"_RAW"] = pwd
+		attrs["PASSWORD_"+pwdName+"_QUOTED"] = strconv.Quote(pwd)
+	}
+
+
+	a.hash = fmt.Sprintf("%x", hash.Sum(nil))
+	return a.hash, nil
+}
+
+func (a *execInjectPasswordsCmd) Exec(l logger.Logger, clients connection.Client) error {
+	sess, err := clients.NewLoggedSession(l)
+	if err != nil {
+		return err
+	}
+	defer sess.Close()
+
+	l.Printf("injecting passwords")
+
+	stdin, err := sess.StdinPipe()
+	if err != nil {
+		return errors.Wrap(err, "failed to receive stdin pipe")
+	}
+
+	cmd := fmt.Sprintf(`/usr/bin/env bash -c "cat - > /tmp/smutje/passwords"`)
+	if err := sess.Start(cmd); err != nil {
+		return err
+	}
+
+	for k, v := range a.values {
+		line := fmt.Sprintf("%s\t%s\n", k, v)
+		_, err := stdin.Write([]byte(line))
+		if err != nil {
+			stdin.Close()
+			return errors.Wrap(err, "failed to write password to target")
+		}
+	}
+	stdin.Close()
+
+	return sess.Wait()
+}
+
+func (a *execInjectPasswordsCmd) MustExecute() bool {
+	return true
+}
+
+func (a *execInjectPasswordsCmd) getPassword(name string) (string, error) {
+	if a.cache == nil {
+		cache := map[string]string{}
+
+		fh, err := os.Open(".passwords")
+		if err != nil {
+			return "", errors.Wrap(err, "failed to read passwords")
+		}
+
+		sc := bufio.NewScanner(fh)
+		for i := 0; sc.Scan(); i++ {
+			line := sc.Text()
+			if strings.TrimSpace(line) == "" {
+				continue
+			}
+			parts := strings.Split(line, ":")
+			if len(parts) != 2 {
+				return "", errors.Errorf("invalid syntax in passwords line %d: expected 2 parts, got %d", i, len(parts))
+			}
+			name, pwd := strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
+			cache[name] = pwd
+		}
+
+		if err := sc.Err(); err != nil {
+			return "", errors.Wrap(err, "failed to parse passwords file")
+		}
+
+		a.cache = cache
+	}
+	pwd, found := a.cache[name]
+	if ! found {
+		return "", errors.Errorf("password %q not found", name)
+	}
+	return pwd, nil
 }
 
 type execJenkinsArtifactCmd struct {
